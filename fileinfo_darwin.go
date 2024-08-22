de483515d23f28f43fab37c25dba2d236c1c01c9
cjj <jianjun.chen@qingteng.cn>
2024-08-22 14:44:14 +0800

    fix macos build

--- fileinfo_darwin.go
+++ fileinfo_darwin.go
@@ -8,6 +8,6 @@ import (
 
 func GetFileCreateTime(fileInfo os.FileInfo) time.Time {
 	// 获取文件的 BirthTime，这是 macOS 上的“创建时间”
-	birthTime := info.Sys().(*syscall.Stat_t).Birthtimespec
+	birthTime := fileInfo.Sys().(*syscall.Stat_t).Birthtimespec
 	return time.Unix(0, int64(birthTime.Sec*1e9+birthTime.Nsec))
 }
